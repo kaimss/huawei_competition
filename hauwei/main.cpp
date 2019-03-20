97944a64a919c84afd6175d61e78634253372101
hands133 <1960303426@qq.com>
2019-03-20 17:08:33 +0800

    将三维数组改为二维数组，增加了三个数据文件，数据文件在源文件的基础上去掉了第一行、所有行的括号和逗号

--- hauwei/main.cpp
+++ hauwei/main.cpp
@@ -10,22 +10,6 @@ using namespace std;
 int main()
 {
 	adjacencyWDigraph graph(64);
-	graph.iniRoad("C:\\huawei\\data1\\road_process.txt");
-	/*
-	car.open("C:\\huawei\\data1\\car_process.txt", ios::in | ios::out|ios::binary);
-	
-	if (!car.is_open()) {
-		cout << "文件打开错误" << endl;
-		exit(0);
-	}
-	char buffer[256];
-	int i = 0;
-	while (!car.eof())
-	{
-		car.getline(buffer, 100);
-		cout << buffer << endl;
-	}
-	car.close();
-	*/
+	graph.iniRoad("data1//road_process.txt");
 	return 0;
 }
